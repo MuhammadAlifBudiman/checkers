d5497646282fe8f94e00518db150a0480bb9c376
Muhammad Alif Budiman <alifm2101@gmail.com>
2025-04-07 13:14:09 +0700

    create the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CheckersComponent } from './components/checkers/checkers.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CheckersComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'checkers';
-}
+export class AppComponent {}
